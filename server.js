38d40a11a93ecf8db355f9aed3a51a7df7322252
bo-bok <bo-bok@users.noreply.github.com>
2017-04-20 13:24:13 +0100

    lnk routes to server.routes func
    relates #9

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var Hapi = require('hapi');
-var server = new Hapi.Server;
+var server = new Hapi.Server();
+
+// var Inert = require('inert');
+
+var routes = require('./src/routes/index.js');
 
 server.connection({
   host: 'localhost',
   port: '4000'
 });
+
+// server.register(Inert, (err) => {
+//   if (err) {
+//     throw err
+//   }
+// });
+
+server.route(routes);
+
 
 server.start( (err) => {
   if (err) {
@@ -14,3 +27,6 @@ server.start( (err) => {
     console.log('server is running on port', server.info.uri)
   }
 })
+
+
+module.export = server;
