06fa930340f625e73cb12c9e5e9634f0bee7e0aa
bo-bok <bo-bok@users.noreply.github.com>
2017-04-21 13:17:48 +0100

    handle the output of TFLCall function in the home route
    relates #11

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,7 +1,17 @@
+var TFLCall = require('../lib/TFLApiCall.js');
+
 module.exports = {
   method: 'GET',
   path: '/',
   handler: (req, reply) => {
-    reply('hello home');
-  }
+    TFLCall((err, res) => {
+      if (err) {
+        return reply('sorry there\'s been an error');
+      }
+      else {
+        return reply('hello home');
+      }
+
+  })
+}
 }
